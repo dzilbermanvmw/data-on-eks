73370b0e8624dd4cf593d3805109569f4e717729
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2023-01-19 12:45:15 -0800

    Update locals.tf
    #DZ for us-east-1 there are 3 AZs, have to restore upper boundary

--- analytics/terraform/spark-k8s-operator/locals.tf
+++ analytics/terraform/spark-k8s-operator/locals.tf
@@ -1,8 +1,8 @@
 locals {
   name          = var.name
   region        = var.region
-  #DZ for us-west-1 there are 2 AZs, have to reduce upper boundary 
-  azs           = slice(data.aws_availability_zones.available.names, 0, 2)
+  #DZ for us-east-1 there are 3 AZs, have to restore upper boundary 
+  azs           = slice(data.aws_availability_zones.available.names, 0, 3)
   vpc_endpoints = ["autoscaling", "ecr.api", "ecr.dkr", "ec2", "ec2messages", "elasticloadbalancing", "sts", "kms", "logs", "ssm", "ssmmessages"]
   spark_team    = "spark-team-a"
 
